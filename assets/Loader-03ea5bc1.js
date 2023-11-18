376340cc4da1dc0326fa81dfdcc6425698e6417a
yukio87 <yukio87@users.noreply.github.com>
2023-11-18 15:30:33 +0000

    deploy: 987b35944565eca80465188d04c9d56f6f22743e

--- assets/Loader-03ea5bc1.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-822f1e8b.js";const r="_loader_1d27t_1",e={loader:r};function a(){return o.jsx("p",{className:e.loader,children:"Loading..."})}export{a as L};
---
+++ assets/Loader-f2f51411.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-b4e6f14f.js";const r="_loader_1d27t_1",e={loader:r};function a(){return o.jsx("p",{className:e.loader,children:"Loading..."})}export{a as L};
